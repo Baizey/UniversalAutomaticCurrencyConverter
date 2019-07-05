e5ec5a3dcc1ec1833976d1c355cea9262091c115
Baizey <Baizey@users.noreply.github.com>
2019-07-05 18:18:08 +0200

    added unit tests

--- tests/Engine.test.js
+++ tests/Engine.test.js
@@ -17,13 +17,33 @@ describe('Engine tests', () => {
             'USD5$',
         ].forEach(text => {
             it(`${text} converts properly`, () => {
-                const expected = [new SearchResult(text, '', '', '', '', [5], 'USD')];
+                const expected = new SearchResult(text, '', '', '', '', [5], 'USD');
 
-                const actual = detector.findAll(text);
+                const actual = detector.findAll(text)[0];
 
-                expect(actual).toEqual(expected);
+                expect(actual.raw).toBe(expected.raw);
+                expect(actual.numbers).toEqual(expected.numbers);
+                expect(actual.currency).toBe(expected.currency);
             });
         });
+        [
+            {number: '5-10', currency: 'USD'},
+            {number: '5-10', currency: 'USD'},
+            {number: '5,600.20', currency: 'USD'},
+        ].forEach(data => {
+            const text = data.number + data.currency;
+            it(`${text} converts properly`, () => {
+                const numbers = data.number.split('-').map(e => e.replace(/[^\d.]/g, '')).map(e => e-0);
+                const expected = new SearchResult(text, '', '', '', '', numbers, data.currency);
+
+                const actual = detector.findAll(text)[0];
+
+                expect(actual.raw).toBe(expected.raw);
+                expect(actual.numbers).toEqual(expected.numbers);
+                expect(actual.currency).toBe(expected.currency);
+            });
+        });
+
     });
 
 });
