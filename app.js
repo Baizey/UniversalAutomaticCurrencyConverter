7a47ad6a85866d5e4bdd34d9e53f23c11796caab
Lasse Spindler Mørk <Baizey@users.noreply.github.com>
2019-12-06 08:51:45 +0100

    update to include endpoints requiring API key

--- app.js
+++ app.js
@@ -82,8 +82,20 @@ class Config {
         return _ConfigInstance ? _ConfigInstance : (_ConfigInstance = new Config(env));
     }
 
-    static get apikey() {
-        return Config.instance()._apikey;
+    /**
+     * @param {string} key
+     * @returns {boolean}
+     */
+    static isValidApiKey(key) {
+        if (!key) return false;
+        return Config.instance()._ownApiKey === key;
+    }
+
+    /**
+     * @returns {string}
+     */
+    static get currencyApiKey() {
+        return Config.instance()._currencyApiKey;
     }
 
     static get port() {
@@ -91,8 +103,9 @@ class Config {
     }
 
     constructor(env) {
+        this._ownApiKey = env.ownApiKey;
         this._port = env.PORT || 3000;
-        this._apikey = env.apikey;
+        this._currencyApiKey = env.apikey;
         Tracker.instance(env.insightkey);
     }
 }
@@ -105,8 +118,8 @@ const data = {
 };
 
 const urls = {
-    symbols: `http://data.fixer.io/api/symbols?access_key=${encodeURIComponent(Config.apikey)}`,
-    rates: `http://data.fixer.io/api/latest?access_key=${encodeURIComponent(Config.apikey)}`
+    symbols: `http://data.fixer.io/api/symbols?access_key=${encodeURIComponent(Config.currencyApiKey)}`,
+    rates: `http://data.fixer.io/api/latest?access_key=${encodeURIComponent(Config.currencyApiKey)}`
 };
 
 const update = async () => {
@@ -115,9 +128,11 @@ const update = async () => {
     data.rates = rates && rates.success ? rates : data.rates;
     data.symbols = symbols && symbols.success ? symbols : data.symbols;
     if (data.rates && data.rates.rates) {
+        delete data.rates.rates['AMD'];
         delete data.rates.rates['ALL'];
     }
     if (data.symbols && data.symbols.symbols) {
+        delete data.rates.rates['AMD'];
         delete data.symbols.symbols['ALL'];
     }
 };
@@ -138,6 +153,27 @@ update().finally(() => {
     api.get('/robots933456.txt', (request, response) => handleRobots(response));
     api.get('/', (request, response) => response.status(200).send(`You're not supposed to be here`));
 
+    api.param('apikey', (request, response, next, key) => {
+        if (Config.isValidApiKey(key)) next();
+        else response.status(401).send('This endpoint requires a valid API key');
+    });
+
+    // Currency rates endpoint
+    api.get('/api/rates/:apikey', (request, response) => {
+        Tracker.in(request, response);
+        return data.rates
+            ? response.status(200).send(data.rates)
+            : response.status(500).send('Dont have any rates');
+    });
+    // Currency symbols endpoint
+    api.get('/api/symbols/:apikey', (request, response) => {
+        Tracker.in(request, response);
+        return data.symbols
+            ? response.status(200).send(data.symbols)
+            : response.status(500).send('Dont have any symbols');
+    });
+
+    // TODO: phase out when most users has updated to a version with API key included
     // Currency rates endpoint
     api.get('/api/rates', (request, response) => {
         Tracker.in(request, response);
