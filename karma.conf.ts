91f148bba76d4fc45ba230131d90ba082780f6d0
Lasse Spindler Mørk <Baizey@users.noreply.github.com>
2021-04-30 17:34:39 +0200

    Update karma.conf.ts

--- karma.conf.ts
+++ karma.conf.ts
@@ -6,23 +6,17 @@ require('karma-firefox-launcher');
 module.exports = function (config: any) {
     config.set({
         files: [
-            'src/Infrastructure/**/*.ts',
-            'src/CurrencyConverter/**/*.ts',
-            'tests/*.mock.ts',
-            'tests/*.test.ts'
+            {pattern: "src/CurrencyConverter/**/*.ts"},
+            {pattern: "src/Infrastructure/**/*.ts"},
+            {pattern: 'tests/*.mock.ts'},
+            {pattern: 'tests/*.test.ts'},
         ],
         preprocessors: {
             "**/*.ts": "karma-typescript",
             "**/*.tsx": "karma-typescript"
         },
         exclude: [],
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        singleRun: true,
-        concurrency: Infinity,
         port: 9876,
-        reporters: ["progress", "karma-typescript"],
-        basePath: "",
         frameworks: ["jasmine", "karma-typescript"],
         browsers: ["ChromeHeadless", "FirefoxHeadless"],
         customLaunchers: {
