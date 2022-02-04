d7081062d6c7c340286684b983b3fee3464c3330
Lasse Spindler Mørk <Baizey@users.noreply.github.com>
2022-02-04 19:07:58 +0100

    .

--- src/content.tsx
+++ src/content.tsx
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom';
 import { useProvider } from './infrastructure';
 import { CurrencyElement } from './currencyConverter/Currency';
 import { LogLevel } from './infrastructure/Logger';
+import { ContentApp } from './components/content';
 
 const isBlacklistedErrorMessage = `Site is blacklisted`;
 
