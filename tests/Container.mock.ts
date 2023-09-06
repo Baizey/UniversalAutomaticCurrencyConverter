486144b781660c38cb5027bb652b47bf33541ea8
Baizey <lassemoerk.mrk@gmail.com>
2023-09-06 18:21:05 +0200

    bump

--- tests/Container.mock.ts
+++ tests/Container.mock.ts
@@ -1,39 +1,41 @@
-import { MockStrategy, singleton } from '@baizey/dependency-injection'
-import { Mockable, setMockProvider, useServices } from '../src/di'
-import { BrowserMock } from './Browser.mock'
-import {Providable} from "../src/provideable";
+import {DependencyInformation, MockStrategy, singleton} from '@baizey/dependency-injection'
+import {Mockable, setMockProvider, useServices} from '../src/di'
+import {Browser} from '../src/infrastructure'
+import {Providable} from '../src/provideable'
+import {BrowserMock} from './Browser.mock'
 
 function addNodeIfNotExisting() {
-	if ( !global.Node ) {
+	if (!global.Node) {
 		// @ts-ignore
 		global.Node = {
 			TEXT_NODE: 3,
-			ELEMENT_NODE: 1
+			ELEMENT_NODE: 1,
 		}
 	}
 }
 
-function mockContainer( mock?: Mockable, defaultStrategy?: MockStrategy ): Providable {
-	useServices().replace({browser: singleton(BrowserMock)})
-	if ( typeof mock === 'string' ) {
+function mockContainer(mock?: Mockable, defaultStrategy?: MockStrategy): Providable {
+	useServices().replace({
+		browser: singleton(BrowserMock) as any as DependencyInformation<Browser, unknown>,
+	})
+	if (typeof mock === 'string') {
 		defaultStrategy = mock as MockStrategy
 		mock = {}
 	}
-	return setMockProvider( {
+	return setMockProvider({
 		browser: MockStrategy.realValue,
 		backendApi: MockStrategy.exceptionStub,
 		backgroundMessenger: MockStrategy.exceptionStub,
 		tabMessenger: MockStrategy.exceptionStub,
-		popupMessenger: MockStrategy.exceptionStub,
-		...mock,
-	}, defaultStrategy )
+		popupMessenger: MockStrategy.exceptionStub, ...mock,
+	}, defaultStrategy)
 }
 
-export function as<A>( obj: any ) {
+export function as<A>(obj: any) {
 	return obj as unknown as A
 }
 
-export default function useMockContainer( mock?: Mockable, defaultStrategy?: MockStrategy ): Providable {
+export default function useMockContainer(mock?: Mockable, defaultStrategy?: MockStrategy): Providable {
 	addNodeIfNotExisting()
-	return mockContainer( mock, defaultStrategy )
+	return mockContainer(mock, defaultStrategy)
 }
