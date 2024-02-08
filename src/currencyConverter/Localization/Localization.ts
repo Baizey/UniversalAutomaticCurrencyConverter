37065acafc54900a2edc2fdd7e9d9690872823a8
KlevtsovAndrey <66443327+KlevtsovAndrey@users.noreply.github.com>
2024-02-08 18:08:36 +0300

    Added currency symbol for Kazashtan tenge (#61)

--- src/currencyConverter/Localization/Localization.ts
+++ src/currencyConverter/Localization/Localization.ts
@@ -55,6 +55,8 @@ export class Localizations {
       INR: ['₹'],
       // Russia
       RUB: ['₽'],
+      // Kazakhstan
+      KZT: ['₸'],
       // Turkey
       TRY: ['₺', 'TL'],
       // Ukraine
