67e94bf0d6192d0c440a78f291ccce43f9184d92
Lasse Spindler Mørk <lassemoerk.mrk@gmail.com>
2024-08-06 19:13:06 +0200

    better mock

--- tests/Container.mock.ts
+++ tests/Container.mock.ts
@@ -1,6 +1,8 @@
-import {Mockable, setMockProvider, useServices} from '../src/di'
+import {Mockable, setMockProvider} from '../src/di'
 import {Providable} from '../src/provideable'
 import {BrowserMock} from './Browser.mock'
+import {Localizations} from "../src/currencyConverter/Localization";
+import {PseudoDom} from "../src/currencyConverter/Detection/pseudoDom";
 
 function addNodeIfNotExisting() {
     if (!global.Node) {
@@ -16,8 +18,30 @@ function mockContainer(mock?: Mockable): Providable {
     mock ??= {}
     return setMockProvider({
         browser: new BrowserMock(),
-        backgroundMessenger: {},
-        tabMessenger: {},
+        backgroundMessenger: {
+            async findCurrencyHolders(dom: PseudoDom): Promise<HTMLElement[]> {
+                const {pseudoFlat} = useMockContainer()
+                return Promise.resolve(pseudoFlat.find(dom.root)
+                    .map(id => dom.element(id))
+                    .filter(e => e) as HTMLElement[])
+            },
+            async getSymbols() {
+                return Promise.resolve(
+                    Localizations.currencySymbols.reduce((a, b) => {
+                        a[b] = b;
+                        return a
+                    }, {})
+                )
+            },
+            async getRate() {
+                throw new Error('Should be overridden by mock')
+            }
+        },
+        tabMessenger: {
+            openContextMenu() {
+                throw new Error('Should not be called')
+            }
+        },
         popupMessenger: {},
         ...mock,
     })
